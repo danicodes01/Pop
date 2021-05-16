f0d473480a87bd4e91365f5476d34b31638ea6e9
MichaelPaulKunz <MichaelPaulKunz@gmail.com>
2021-05-16 13:24:58 -0500

    Revert "Removed some warnings in App.jsx and console.logs."

--- client/src/components/UserProfileView/UserProfile.jsx
+++ client/src/components/UserProfileView/UserProfile.jsx
@@ -15,14 +15,12 @@ margin: 53px 0 6px ;
 const UserProfile = ({ user }) => {
   const [editWindowOpen, setEditWindowOpen] = useState(false);
   const {t} = useTranslation()
-  
+console.log(user)
   return (
     <div>
       <h1>{t("userProfileTxt")}</h1>
       <hr/>
       <Div2></Div2>
-      {console.log(user)}
-      {/* useEffect(() => merchant ? setCoord() : null, [merchant]); */}
       <img src={user.picture}/>
       <br/>
       <br/>
